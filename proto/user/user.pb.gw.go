75f69cb60a0281450da702433f52310bf66e719d
Augustin Godiscal <hello@auggod.tech>
2022-02-08 20:22:27 +0100

    Change endpoint (replaces _ with -)

--- proto/user/user.pb.gw.go
+++ proto/user/user.pb.gw.go
@@ -1456,7 +1456,7 @@ var (
 
 	pattern_ResonateUser_ListUsers_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "v1", "users"}, ""))
 
-	pattern_ResonateUser_AddUserUploadSubmission_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "v1", "users", "id", "upload_submission"}, ""))
+	pattern_ResonateUser_AddUserUploadSubmission_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "v1", "users", "id", "upload-submission"}, ""))
 
 	pattern_ResonateUser_AddUserGroup_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"api", "v1", "users", "id", "usergroup"}, ""))
 
