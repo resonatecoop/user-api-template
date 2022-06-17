6b5082d59f271a77b777eee835900f05a67b2ece
Augustin Godiscal <hello@auggod.tech>
2022-06-17 13:32:45 +0200

    Remove resonate specific code additions (models, legacy models, generated client, ...)

--- model/membership_class.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// MembershipClass
-type MembershipClass struct {
-	IDRecord
-	Name      string `bun:",notnull"`
-	ProductID string `bun:",notnull"`
-	PriceID   string `bun:",notnull"`
-}
